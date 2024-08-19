2d651a1576cced3e9d46b15d3adf192f49956a16
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-08-19 11:35:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 
 module "ecr" {
   source                 = "./ecr"
-  game_server_name_image = va.game_server_name_image
+  game_server_name_image = var.game_server_name_image
   proxy_name_image       = var.proxy_name_image
 }
 
