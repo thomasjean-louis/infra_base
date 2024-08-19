8c7d0c522c2cdb21cb9117bc5cf2d9536b9ff4c8
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-08-19 15:05:11 +0200

    Update ecr.tf

--- ecr/ecr.tf
+++ ecr/ecr.tf
@@ -25,31 +25,12 @@ resource "aws_ecr_repository" "proxy_ecr_repo" {
   image_tag_mutability = "IMMUTABLE"
 }
 
-## IAM user + roles required for ECR management
+## IAM user + inline policies required for ECR management
 resource "aws_iam_user" "terraform_infra_user" {
   name = "ecr_user_${var.deployment_branch}"
 
   tags = {
     name = "ecr_user_${var.deployment_branch}"
-  }
-}
-
-resource "aws_iam_role" "terra_form_infra_role" {
-  name = "ecr_role_${var.deployment_branch}"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        "Effect" : "Allow",
-        "Action" : "sts:AssumeRole",
-        "Principal" : { "AWS" : "arn:aws:iam::${var.account_id}:user/${aws_iam_user.terraform_infra_user.name}" },
-      },
-    ]
-  })
-
-  tags = {
-    name = "ecr_role_${var.deployment_branch}"
   }
 }
 
