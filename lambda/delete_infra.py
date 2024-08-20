dc5719ab3eefdb144d0ff1fa77f6bbc877eba450
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-08-20 17:20:03 +0200

    Update delete_infra.py

--- lambda/delete_infra.py
+++ lambda/delete_infra.py
@@ -1,13 +1,16 @@
-import requests
+import urllib.parse
+import urllib.request
 import os
 
 def lambda_handler(event, context):
+  data = urllib.parse.urlencode({'ref': os.environ["DEPLOYMENT_BRANCH"]})
+  data = data.encode('ascii')
 
-  headers = {
-    'Accept': 'application/vnd.github+json',
-    'Authorization': 'Bearer'+os.environ["TOKEN_GITHUB"],
-    'X-GitHub-Api-Version': '2022-11-28'
-  }  
+  req = urllib.request.Request('https://api.github.com/repos/thomasjean-louis/infra/actions/workflows/deleteResources.yml/dispatches', data=data) 
+  req.add_header('Accept', 'application/vnd.github+json')
+  req.add_header('Authorization', 'Bearer'+os.environ["TOKEN_GITHUB"])
+  req.add_header('X-GitHub-Api-Version', '2022-11-28')
+  req.get_method = lambda: 'POST'
 
-  data = '{"ref":"os.environ["DEPLOYMENT_BRANCH"]"}'
-  res = requests.post(' https://api.github.com/repos/thomasjean-louis/infra/actions/workflows/deleteResources.yml/dispatches', headers=headers, data=data)
+  with urllib.request.urlopen(req) as f:
+    print(f.read().decode('utf-8'))
