b87a2098923aa7fcaaa88a82621da2285f66d102
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-08-21 10:39:59 +0200

    Update lambda.tf

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -107,7 +107,7 @@ resource "aws_lambda_permission" "allow_eventbridge_delete" {
 resource "aws_cloudwatch_event_rule" "create_infra_rule" {
   name        = "create_infra_rule"
 
-  schedule_expression = "cron(38 8 ? * MON-FRI *)"
+  schedule_expression = "cron(41 8 ? * MON-FRI *)"
 }
 
 
