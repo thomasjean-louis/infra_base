f651fda7792a384917fbc4dc5e37ba0028a8825d
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-08-23 15:24:30 +0200

    Update delete_infra.py

--- lambda/delete_infra.py
+++ lambda/delete_infra.py
@@ -6,27 +6,6 @@ import boto3
 
 def lambda_handler(event, context):
   
-  # # Destroy terraform stack
-  # url = "https://api.github.com/repos/thomasjean-louis/infra/actions/workflows/deleteResources.yml/dispatches"
-  
-  # values = {"ref": os.environ["DEPLOYMENT_BRANCH"],}
-  # headers = {
-  #   "Accept": "application/vnd.github+json",
-  #   "Authorization": "Bearer "+os.environ["TOKEN_GITHUB"],
-  #   "X-GitHub-Api-Version": "2022-11-28",
-  # }
-  
-  # data = json.dumps(values).encode("utf-8")
-  # pprint(data)
-
-  # try:
-  #   req = urllib.request.Request(url, data, headers)
-  #   with urllib.request.urlopen(req) as f:
-  #       res = f.read()
-  #   pprint(res.decode())
-  # except Exception as e:
-  #   pprint(e)
-
   # Remove acm-validation Records
   client53 = boto3.client('route53')
   response53 = client53.list_resource_record_sets(
@@ -60,6 +39,29 @@ def lambda_handler(event, context):
          },
         HostedZoneId = os.environ["HOSTED_ZONE_ID"],
       )
+
+  # # Destroy terraform stack
+  url = "https://api.github.com/repos/thomasjean-louis/infra/actions/workflows/deleteResources.yml/dispatches"
+  
+  values = {"ref": os.environ["DEPLOYMENT_BRANCH"],}
+  headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": "Bearer "+os.environ["TOKEN_GITHUB"],
+    "X-GitHub-Api-Version": "2022-11-28",
+  }
+  
+  data = json.dumps(values).encode("utf-8")
+  pprint(data)
+
+  try:
+    req = urllib.request.Request(url, data, headers)
+    with urllib.request.urlopen(req) as f:
+        res = f.read()
+    pprint(res.decode())
+  except Exception as e:
+    pprint(e)
+
+  
 
       
 
