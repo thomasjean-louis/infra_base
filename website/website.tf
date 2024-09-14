c897e329c1f3578d2cd500d2c9d819ac6915c8c0
thomas <159631593+thomasjean-louis@users.noreply.github.com>
2024-09-14 18:30:25 +0200

    Update website.tf

--- website/website.tf
+++ website/website.tf
@@ -10,6 +10,13 @@ variable "website_name" {
   type = string
 }
 
+resource "random_string" "random_string" {
+  length  = 10
+  special = false
+  numeric = false
+  upper   = false
+}
+
 resource "aws_s3_bucket" "website-bucket" {
   bucket = "s3-${var.region}-${var.website_name}-${random_string.random_string.result}-${var.deployment_branch}"
 
