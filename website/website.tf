2082ce45c00fad80934ea9b87b03a57249f8ab2b
thomas <159631593+thomasjean-louis@users.noreply.github.com>
2024-09-15 17:49:45 +0200

    Update website.tf

--- website/website.tf
+++ website/website.tf
@@ -101,6 +101,34 @@ resource "aws_cloudfront_distribution" "distribution" {
 
 }
 
+# ACM certificate
+resource "aws_acm_certificate" "website_certificate" {
+  domain_name       = var.hosted_zone_name
+  validation_method = "DNS"
+}
+
+resource "aws_route53_record" "dns_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.website_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.hosted_zone_id
+}
+
+resource "aws_acm_certificate_validation" "api_domaine_name_certificate_validation" {
+  certificate_arn         = aws_acm_certificate.website_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.dns_record : record.fqdn]
+}
+
 # Cloudfront alias
 resource "aws_route53_record" "cloudfront_alias" {
   zone_id = var.hosted_zone_id
