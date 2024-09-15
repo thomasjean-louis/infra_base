ac38903568a408df460a0b0a059f75d116fdbeaa
thomas <159631593+thomasjean-louis@users.noreply.github.com>
2024-09-15 16:08:32 +0200

    Update website.tf

--- website/website.tf
+++ website/website.tf
@@ -36,3 +36,77 @@ resource "aws_s3_bucket_website_configuration" "configuration" {
     key = "error.html"
   }
 }
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.website-bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "AllowGetObjects"
+    Statement = [
+      {
+        Sid       = "AllowPublic"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website-bucket.arn}/**"
+      }
+    ]
+  })
+}
+
+# Cloudfront distribution
+
+locals {
+  s3_origin_id   = "${aws_s3_bucket.website-bucket.bucket}-origin"
+  s3_domain_name = "${aws_s3_bucket.website-bucket.bucket}.s3-website-${var.region}.amazonaws.com"
+}
+
+resource "aws_cloudfront_distribution" "distribution" {
+
+  enabled = true
+
+  origin {
+    origin_id   = local.s3_origin_id
+    domain_name = local.s3_domain_name
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1"]
+    }
+  }
+
+  default_cache_behavior {
+
+    target_origin_id = local.s3_origin_id
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  price_class = "PriceClass_100"
+
+}
