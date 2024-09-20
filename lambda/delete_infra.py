1d604b4bbce01ab1033b24255097981ffae87e44
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-09-20 11:42:32 +0200

    Update delete_infra.py

--- lambda/delete_infra.py
+++ lambda/delete_infra.py
@@ -67,26 +67,26 @@ def lambda_handler(event, context):
         HostedZoneId = os.environ["HOSTED_ZONE_ID"],
       )
 
-  # # Destroy terraform stack
-  # url = "https://api.github.com/repos/thomasjean-louis/infra/actions/workflows/deleteResources.yml/dispatches"
+  # Destroy terraform stack
+  url = "https://api.github.com/repos/thomasjean-louis/infra/actions/workflows/deleteResources.yml/dispatches"
   
-  # values = {"ref": os.environ["DEPLOYMENT_BRANCH"],}
-  # headers = {
-  #   "Accept": "application/vnd.github+json",
-  #   "Authorization": "Bearer "+os.environ["TOKEN_GITHUB"],
-  #   "X-GitHub-Api-Version": "2022-11-28",
-  # }
+  values = {"ref": os.environ["DEPLOYMENT_BRANCH"],}
+  headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": "Bearer "+os.environ["TOKEN_GITHUB"],
+    "X-GitHub-Api-Version": "2022-11-28",
+  }
   
-  # data = json.dumps(values).encode("utf-8")
-  # pprint(data)
+  data = json.dumps(values).encode("utf-8")
+  pprint(data)
 
-  # try:
-  #   req = urllib.request.Request(url, data, headers)
-  #   with urllib.request.urlopen(req) as f:
-  #       res = f.read()
-  #   pprint(res.decode())
-  # except Exception as e:
-  #   pprint(e)
+  try:
+    req = urllib.request.Request(url, data, headers)
+    with urllib.request.urlopen(req) as f:
+        res = f.read()
+    pprint(res.decode())
+  except Exception as e:
+    pprint(e)
 
   
 
